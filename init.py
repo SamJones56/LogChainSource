f2403f6b65743a3b031f38467b807bb9fea8cb72
SamJones56 <b00156001@mytudublin.ie>
2025-03-06 09:58:56 +0000

    Initial Commit

---
+++ init.py
@@ -0,0 +1,86 @@
+import subprocess, json
+
+# https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+#### Create the docker network ####
+# Delete current network if exists
+def netDelete():
+    print(bcolors.WARNING + "Removing old netwokrs" + bcolors.WARNING)
+    cmd = ["docker", "network", "rm", "Multichain-Network"]
+    return cmd
+
+# Create new network
+def netCreate():
+    print(bcolors.OKBLUE + "Name: Multichain-network, Subnet: 192.168.1.0/24" + bcolors.ENDC)
+    cmd = ["docker", "network", "create", "-d", "overlay", "--attachable", "--subnet","192.168.1.0/24","Multichain-Network"]
+    return cmd
+
+# Network Generation Commands
+netGen = [netDelete, netCreate]
+
+#### Docker Image Setup ####
+# Delete Old Image
+def imageDelete():
+    print(bcolors.WARNING + "Removing old images" + bcolors.WARNING)
+    cmd = ["docker", "rmi", "multichain_image", "."]
+    return cmd
+ 
+# Build New Docker Image
+def imageCreate():
+    print(bcolors.OKBLUE + "Creating image" + bcolors.ENDC)
+    cmd = ["docker", "build", "-t", "multichain_image", "."]
+    return cmd
+
+# Image Generation Commands
+imageGen = [imageDelete, imageCreate]
+
+#### Docker Container Setup ####
+# host count for ips
+hosts = 0
+
+# Delete old containers
+def contClear():
+    print(bcolors.WARNING + "Removing old containers" + bcolors.WARNING)
+    cmd = ["docker", "rm", "$(docker ps -q)"]
+    return cmd
+
+# Create Genesis node
+def genesisCreate():
+    global hosts
+    hosts += 1
+    print(bcolors.OKBLUE + "Creating Genesis: IP: 192.168.1.2, Ports - 6000,6001,6002" + bcolors.ENDC)
+    with open("cont.json", "r") as file:
+        data = json.load(file)
+        return data.get("genesis", [])
+
+# Create Node1
+def node1Create():
+    global hosts
+    hosts += 1
+    print(bcolors.OKBLUE + "Creating Node1: IP: 192.168.1.3 Ports - 6010,6011,6012" + bcolors.ENDC)
+    with open("cont.json", "r") as file:
+        data = json.load(file)
+        return data.get("node1", [])
+
+
+# Container Generation Commands
+contGen = [contClear, genesisCreate, node1Create]
+
+
+commands = [netGen, imageGen, contGen]
+
+# Run Commands
+for cmd_group in commands:
+    for cmd in cmd_group:
+        print(bcolors.OKGREEN + f"Running {cmd.__name__}..." + bcolors.ENDC)
+        subprocess.run(cmd())
