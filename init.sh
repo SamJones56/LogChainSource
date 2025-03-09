2dd1f616a208633a26a62c794c63d74151dc4321
SamJones56 <B00156001@mytudublin.ie>
2025-03-09 09:25:22 +0000

    Work on RPC

--- init.sh
+++ init.sh
@@ -12,22 +12,22 @@ mkdir ../multichain/node1_data/
 echo "Building Docker"
 docker compose up --build -d
 
-# Wait for docker
-echo "waiting 60s for docker to build"
-sleep 60
+# # Wait for docker
+# echo "waiting 60s for docker to build"
+# sleep 60
 
-# Get debug.log info
-genesisContainer="multichain_genesis"
-node1Container="multichain_node1"
+# # Get debug.log info
+# genesisContainer="multichain_genesis"
+# node1Container="multichain_node1"
 
-echo "Pulling node1 address"
-node1addr=$(docker exec $node1Container grep "Minimal blockchain parameter set is created, default address: " /root/.multichain/logChain/debug.log | awk '{print $NF}')
-echo "Node1 address = $node1addr"
+# echo "Pulling node1 address"
+# node1addr=$(docker exec $node1Container grep "Minimal blockchain parameter set is created, default address: " /root/.multichain/logChain/debug.log | awk '{print $NF}')
+# echo "Node1 address = $node1addr"
 
-# execute on genesis
-echo "executing connection on genesis"
-docker exec $genesisContainer multichain-cli logChain grant $node1addr connect,send,receive
+# # execute on genesis
+# echo "executing connection on genesis"
+# docker exec $genesisContainer multichain-cli logChain grant $node1addr connect,send,receive
 
-# execute on node1
-echo "starting node1 daemon"
-docker exec $node1Container multichaind logChain -daemon
+# # execute on node1
+# echo "starting node1 daemon"
+# docker exec $node1Container multichaind logChain -daemon
