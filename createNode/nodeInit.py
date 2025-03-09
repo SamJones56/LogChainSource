9a867f229e9105b6584d575782ca145769965874
SamJones56 <B00156001@mytudublin.ie>
2025-03-09 15:03:48 +0000

    work on getting address

--- createNode/nodeInit.py
+++ createNode/nodeInit.py
@@ -16,15 +16,6 @@ def demChain():
     print(bcolors.OKGREEN + "multichaind logChain@172.18.0.2:7010 -daemon" + bcolors.ENDC)
     cmd = ["multichaind", "logChain@172.18.0.2:7010", "-daemon"]
     subprocess.run(cmd)
-    # time.sleep(10)
-    # addrPath = "/root/.multichain/logChain/debug.log"
-    # walletAddr = ""
-    # with open(addrPath, "r") as f:
-    #     for line in f:
-    #         if line.startswith("Minimal blockchain parameter set is created, default address:"):
-    #             walletAddr = line.split(":")[-1].strip()
-    #             print("--------- WALLET ADDR ---------")
-    #             print(walletAddr)
-    #             break
+
 
 demChain()
--- test.py
+++ test.py
@@ -7,18 +7,3 @@ rpcuser='genesis' # see multichain.conf in blockchain directory
 rpcpassword='logChain' # see multichain.conf in blockchain directory
 
 mc=MultiChainClient(rpchost, rpcport, rpcuser, rpcpassword)
-
-address = "root/.multichain/logChain/wallet.dat"
-walletAddr = ""
-with open(address, "r") as f:
-    for line in f:
-        if line.startswith("Minimal blockchain parameter set is created, default address:"):
-            walletAddr = line.split(":")[-1].strip()
-            print("--------- WALLET ADDR ---------")
-            print(walletAddr)
-            break
-
-# txid=mc.grant()
-#curl -v --fail --user genesis:logChain 
-# --data-binary '{ "method" : "grant", "params" : ["19DsJCDPFzMLnG66mMm94sPtLzGNPDoD77NHKx","connect,send,receive"], "id":"1", "jsonrpc":"2.0"}' 
-# -H 'content-type: text/plain;' http://172.18.0.2:7011 
