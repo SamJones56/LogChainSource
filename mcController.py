ad3868b7085a5be462f80fa8419f02161fb8a7ae
SamJones56 <B00156001@mytudublin.ie>
2025-03-10 23:35:16 +0000

    work on stream gen

--- mcController.py
+++ mcController.py
@@ -8,13 +8,13 @@ rpchost = '172.18.0.2'
 rpcport = '7011'         
 
 # Setup client
-client = MultiChainClient(rpchost, rpcport, rpcuser, rpcpassword)
+mc = MultiChainClient(rpchost, rpcport, rpcuser, rpcpassword)
 
 # Take wallet address as input and connect to genesis node
 def connectToChain(address):
     permissions = "connect,send,receive"
     try:
-        response = client.grant(address, permissions)
+        response = mc.grant(address, permissions)
         print("Grant status: " + response + ". Starting")
         subprocess.run(["multichaind", "logChain", "-daemon"])
     except Exception as e:
@@ -23,8 +23,17 @@ def connectToChain(address):
 # Create a stream -> give name + restrictions in JSON format
 def createStream(name, restrictions):
     # type = "type=" + name
-    try:
-        response = client.create("type=stream", name, restrictions)
-        print("Stream status: ", response)
-    except Exception as e:
-        print("Error granting permissions:", str(e))
+    # try:
+    #     response = mc.create("type=stream", name, restrictions)
+    #     print("Stream status: ", response)
+    # except Exception as e:
+    #     print("Error granting permissions:", str(e))
+    txid=mc.create('stream', name, restrictions)
+
+    if mc.success():
+        print("Stream: ", name, " successful")
+        pass # operation was successful
+
+    else: # operation failed
+        print('Error code: '+str(mc.errorcode())+'\n')
+        print('Error message: '+mc.errormessage()+'\n')
