1d44a64c0af6305a7816c4822e3368f9da1b3fad
SamJones56 <B00156001@mytudublin.ie>
2025-03-11 08:29:46 +0000

    work on init

--- mcController.py
+++ mcController.py
@@ -10,29 +10,39 @@ rpcport = '7011'
 # Setup client
 mc = MultiChainClient(rpchost, rpcport, rpcuser, rpcpassword)
 
-# Take wallet address as input and connect to genesis node
-def connectToChain(walletAddress):
-    permissions = "connect,send,receive"
-    print(mc.grant(walletAddress, permissions))
-    if mc.success():
-        print("Chain: ", walletAddress, " successful")
-        pass # operation was successful
-    else:
-        print('Error code: '+str(mc.errorcode())+'\n')
-        print('Error message: '+mc.errormessage()+'\n')
-
-
-# Create a stream -> give name + restrictions in JSON format
-def createStream(streamName, streamRestrictions):
-    print(mc.create('stream', streamName, streamRestrictions))
-    
-    for i in range(120):
+def connect(mc):
+    for i in range(60):
         if mc.success():
-            print("Stream: ", streamName, " successful")
+            print("Successful")
             break # operation was successful
         time.sleep(1)
         print('Error code: '+str(mc.errorcode())+'\n')
         print('Error message: '+mc.errormessage()+'\n')
+
+# Take wallet address as input and connect to genesis node
+def connectToChain(walletAddress):
+    permissions = "connect,send,receive"
+    connect(mc.grant(walletAddress, permissions))
+    # print(mc.grant(walletAddress, permissions))
+    # if mc.success():
+    #     print("Chain: ", walletAddress, " successful")
+    #     pass # operation was successful
+    # else:
+    #     print('Error code: '+str(mc.errorcode())+'\n')
+    #     print('Error message: '+mc.errormessage()+'\n')
+
+
+# Create a stream -> give name + restrictions in JSON format
+def createStream(streamName, streamRestrictions):
+    connect(mc.create('stream', streamName, streamRestrictions))
+    # print(mc.create('stream', streamName, streamRestrictions))
+    # for i in range(120):
+    #     if mc.success():
+    #         print("Stream: ", streamName, " successful")
+    #         break # operation was successful
+    #     time.sleep(1)
+    #     print('Error code: '+str(mc.errorcode())+'\n')
+    #     print('Error message: '+mc.errormessage()+'\n')
 
 
 # Subscribe to existing stream
