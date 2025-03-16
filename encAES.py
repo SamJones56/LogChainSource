c0527d54fec14fd2400336be3863cb6bb5d93f3d
SamJones56 <B00156001@mytudublin.ie>
2025-03-16 12:22:55 +0000

    work on crypto

--- encAES.py
+++ encAES.py
@@ -1,12 +1,8 @@
-from Crypto.Cipher import AES
-import os
-
 def encAes(data):
-        data.encode()
-        key = os.urandom(32)
-        cipher = AES.new(key, AES.MODE_EAX)
+        encKey = os.urandom(32)
+        cipher = AES.new(encKey, AES.MODE_EAX)
 
         nonce = cipher.nonce
         ciphertext, tag = cipher.encrypt_and_digest(data)
 
-        return(key.hex(),nonce.hex(),ciphertext.hex())
+        return(encKey,nonce,ciphertext)
--- winLogReader.py
+++ winLogReader.py
@@ -1,6 +1,7 @@
 from mcController import connectToChain, subStream, grantStream, addToStream ,getStreamData
 import csv
 import time
+import json
 # https://stackoverflow.com/questions/3996904/generate-random-integers-between-0-and-9
 from random import randrange
 # https://pycryptodome.readthedocs.io/en/latest/src/cipher/aes.html
@@ -36,13 +37,69 @@ with open('winTest.csv', newline='') as csvfile:
                        "EventTemplate":row['EventTemplate'],
                        }}
         # Encrypt the log and return key, nonce, and ciphertext
-        key,nonce,ciphertext = encAes(log)
-
-        # Encrypt the 
-
-        # Print out what we are doing
+	# json to binary for encryption
+        stringLog=json.dumps(log)
+        binaryLog=stringLog.encode('utf-8')
+	# Save encrypted data
+        encKey,nonce,ciphertext = encAes(binaryLog)
+	# Convert to hex for saving to chain
+        enckey = encKey.hex()
+        nonce = nonce.hex()
+        ciphertext = ciphertext.hex()
+	# Encrypt the aes key using the genesis pk
+	
+	# Data for posting to data stream
+        data = {"json":{"nonce":nonce, "data":ciphertext}}
+        # Add to the data stream
         print(bcolors.WARNING + "Ammending ", end=" ")
         print(log, end=" ")
         print(" to Chain" + bcolors.ENDC)
-        addToStream(streamName, key, ct)
+        addToStream(streamName, key, data)
+# from mcController import connectToChain, subStream, grantStream, addToStream ,getStreamData
+# import csv
+# import time
+# # https://stackoverflow.com/questions/3996904/generate-random-integers-between-0-and-9
+# from random import randrange
+# # https://pycryptodome.readthedocs.io/en/latest/src/cipher/aes.html
+# from encAES import encAes
+# # LineId,Date,Time,Level,Component,Content,EventId,EventTemplate
+
+# pkFile="kPk.key"
+
+# # Colours for printing
+# class bcolors:
+#     WARNING = '\033[93m'
+#     ENDC = '\033[0m'
+
+# streamName = "data"
+# key = "node1"
+
+# # https://docs.python.org/3/library/csv.html
+# # Parse through the csv
+# with open('winTest.csv', newline='') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         # Simulate random time
+#         t = randrange(6)
+#         time.sleep(t)
+#         # Generate the json file
+#         log = {"json":{"LogId" : row['LineId'],
+#                        "Date":row['Date'],
+#                        "Time":row['Time'],
+#                        "Level":row['Level'],
+#                        "Component":row['Component'],
+#                        "Content":row['Content'],
+#                        "EventId":row['EventId'],
+#                        "EventTemplate":row['EventTemplate'],
+#                        }}
+#         # Encrypt the log and return key, nonce, and ciphertext
+#         key,nonce,ciphertext = encAes(log)
+
+#         data = {"json":{"nonce":"{nonce}", "data":"{ciphertext}"}}
+
+#         # Print out what we are doing
+#         print(bcolors.WARNING + "Ammending ", end=" ")
+#         print(log, end=" ")
+#         print(" to Chain" + bcolors.ENDC)
+#         addToStream(streamName, key, data)
 
