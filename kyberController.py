53e7080b14014e5450391c51737e992ff7c4a9a0
SamJones56 <B00156001@mytudublin.ie>
2025-03-16 14:04:25 +0000

    work on crypto

--- kyberController.py
+++ kyberController.py
@@ -1,46 +1,46 @@
 # This python class will be used to generate and post to stream the kyber public key
 # https://github.com/aabmets/quantcrypt/wiki/Code-Examples
-from quantcrypt.kem import Kyber
-import os
+# from quantcrypt.kem import Kyber
+# import os
 
-kyber = Kyber()
+# kyber = Kyber()
 
-class bcolors:
-    OKGREEN = '\033[92m'
-    ENDC = '\033[0m'
+# class bcolors:
+#     OKGREEN = '\033[92m'
+#     ENDC = '\033[0m'
 
-# file locations
-publicKeyFile="kPk.key"
-secretKeyFile="kSk.key"
+# # file locations
+# publicKeyFile="kPk.key"
+# secretKeyFile="kSk.key"
 
-# Writing data to a file
-def writeToFile(file, data):
-    with open(file,"wb") as f:
-        f.write(data)
+# # Writing data to a file
+# def writeToFile(file, data):
+#     with open(file,"wb") as f:
+#         f.write(data)
 
-# Get pubkey
-def readFromFile(file):
-    with open(file,"rb") as f:
-        return f.read()
+# # Get pubkey
+# def readFromFile(file):
+#     with open(file,"rb") as f:
+#         return f.read()
 
-# # Generate and save private/public keys ~ TODO make files private
-def genKeys():
-    # Generate kyber keys
-    pk,sk = kyber.keygen()
-    # Write keys to file
-    writeToFile(publicKeyFile, pk)
-    writeToFile(secretKeyFile, sk)
-    print(bcolors.OKGREEN + "Wrote keys to files: " + publicKeyFile + secretKeyFile + bcolors.ENDC)
+# # # Generate and save private/public keys ~ TODO make files private
+# def genKeys():
+#     # Generate kyber keys
+#     pk,sk = kyber.keygen()
+#     # Write keys to file
+#     writeToFile(publicKeyFile, pk)
+#     writeToFile(secretKeyFile, sk)
+#     print(bcolors.OKGREEN + "Wrote keys to files: " + publicKeyFile + secretKeyFile + bcolors.ENDC)
 
 
-# Encrypt data
-def encryptData():
-    publicKey = readFromFile(publicKeyFile)
-    cipherText, sharedSecret = kyber.encaps(publicKey)
-    return cipherText,sharedSecret
+# # Encrypt data
+# def encryptData():
+#     publicKey = readFromFile(publicKeyFile)
+#     cipherText, sharedSecret = kyber.encaps(publicKey)
+#     return cipherText,sharedSecret
 
-# Decrypt data
-def decryptData(cipherText):
-    secretKey = readFromFile(secretKeyFile)
-    sharedSecret = kyber.decaps(secretKey, cipherText)
-    return sharedSecret
+# # Decrypt data
+# def decryptData(cipherText):
+#     secretKey = readFromFile(secretKeyFile)
+#     sharedSecret = kyber.decaps(secretKey, cipherText)
+#     return sharedSecret
