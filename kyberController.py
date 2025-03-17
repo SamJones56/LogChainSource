0addf1ea20a98087bf710a8307a09727e364199d
SamJones56 <B00156001@mytudublin.ie>
2025-03-17 10:00:37 +0000

    work on crypto

--- kyberController.py
+++ kyberController.py
@@ -1,8 +1,9 @@
 # This python class will be used to generate and post to stream the kyber public key
+# https://github.com/aabmets/quantcrypt
 # https://github.com/aabmets/quantcrypt/wiki/Code-Examples
-from quantcrypt.kem import MLKEM_512
+from quantcrypt.kem import MLKEM_1024
 
-kyber = MLKEM_512()
+kyber = MLKEM_1024()
 
 class bcolors:
     OKGREEN = '\033[92m'
