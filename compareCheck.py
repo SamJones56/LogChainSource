14650e61b931f2081fbf61fb8466d69635fbc897
SamJones56 <B00156001@mytudublin.ie>
2025-03-21 09:40:20 +0000

    work on comprehension

--- compareCheck.py
+++ compareCheck.py
@@ -1,20 +1,24 @@
 import json
 from collections import Counter
 
-
+# Filter the logs
 def filterLog(jsonOut, dataName):
     return [jData for jData in jsonOut if jData["data"]["Type"]==dataName]
 
+# Get log id's
 def addId(log, dataName):
-    id = []
-    for jLine in log:
-            if dataName == "LogId":
-                if jLine["data"][dataName] not in id:
-                    return(jLine["data"][dataName])
-            elif dataName == "LineId":
-                if jLine["data"][dataName] not in id:
-                    return(jLine["data"][dataName])
-            
+    # id = []
+    # for jLine in log:
+    #         if dataName == "LogId":
+    #             if jLine["data"][dataName] not in id:
+    #                 return(jLine["data"][dataName])
+    #         elif dataName == "LineId":
+    #             if jLine["data"][dataName] not in id:
+    #                 return(jLine["data"][dataName])
+    ids = [jLine["data"][dataName] for jLine in log]
+    print(ids)
+    return Counter(id["data"][dataName] for id in ids)
+
 #def contOccurance
 
 # https://likegeeks.com/count-json-array-elements-python/#:~:text=7%20Benchmark%20Test-,Using%20len()%20Function,arrays%20>
@@ -25,26 +29,16 @@ def logCompare(fileName):
     with open(fileName,"r") as f:
         # Read from json
         jsonOut = json.load(f)
-        wId = []
-        lId = []
 
         # Filter logs
         winLogs = filterLog(jsonOut,"Windows")
         linLogs = filterLog(jsonOut,"Linux")
-
-
+        # Gather Id's
+        winLogIds = addId(winLogs, "LogId")
+        linLogIds = addId(linLogs, "LineId")
+        print("Win Id's", winLogIds)
+        #Id count
+        # winIdCount = Counter(jline["data"]["LogId"] for jline in winLogs)
         # Declare list of id's ~ This can be used to check for what should be there
-
-
-        # # Loop through JSON lines in JSON output
-        # for jLine in jsonOut:
-        #     # Check if windows
-        #     if jLine["data"]["Type"] == "Windows":
-        #         if jLine["data"]["LogId"] not in ids:
-        #             ids.append(jLine["data"]["LogId"])
-        #             print(ids)
-        #             test = "1"
-        #             count = sum(1 for obj in jsonOut if obj["data"]["LogId"] == test)
-        #             print(count)
-        #     elif jLine["data"]["Type"] == "Linux":
-        #         break
+        
+        # print(winIdCount)
