a3e3ad6665480e8aa02806fd59c4c1df81690468
SamJones56 <B00156001@mytudublin.ie>
2025-03-21 09:44:11 +0000

    work on comprehension

--- compareCheck.py
+++ compareCheck.py
@@ -5,19 +5,10 @@ from collections import Counter
 def filterLog(jsonOut, dataName):
     return [jData for jData in jsonOut if jData["data"]["Type"]==dataName]
 
-# Get log id's
+# Get count of log id's
 def addId(log, dataName):
-    # id = []
-    # for jLine in log:
-    #         if dataName == "LogId":
-    #             if jLine["data"][dataName] not in id:
-    #                 return(jLine["data"][dataName])
-    #         elif dataName == "LineId":
-    #             if jLine["data"][dataName] not in id:
-    #                 return(jLine["data"][dataName])
     ids = [jLine["data"][dataName] for jLine in log]
-    print(ids)
-    return Counter(id["data"][dataName] for id in ids)
+    return Counter(ids)
 
 #def contOccurance
 
@@ -33,12 +24,8 @@ def logCompare(fileName):
         # Filter logs
         winLogs = filterLog(jsonOut,"Windows")
         linLogs = filterLog(jsonOut,"Linux")
-        # Gather Id's
+        # Gather count of Id's
         winLogIds = addId(winLogs, "LogId")
         linLogIds = addId(linLogs, "LineId")
         print("Win Id's", winLogIds)
-        #Id count
-        # winIdCount = Counter(jline["data"]["LogId"] for jline in winLogs)
-        # Declare list of id's ~ This can be used to check for what should be there
-        
-        # print(winIdCount)
+        print("Linux Id's", linLogIds)
