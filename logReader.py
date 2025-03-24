d4bf0c38f38cc46bf989b99f18095a10827db26e
SamJones56 <B00156001@mytudublin.ie>
2025-03-24 18:37:37 +0000

    work on log reader rewrite

--- logReader.py
+++ logReader.py
@@ -9,7 +9,24 @@ import time
 def readDecryptSave(fileName, streamName):
     # Get stream data
     streamData = getStreamData(streamName, False)
+    # init output
+    output = []
     # Get json data
+    for line in streamData:
+        kyberct = line["data"]["kyberct"]
+        nonce = line["data"]["nonce"]
+        cipherText = line["data"]["log"]
+        tag = line["data"]["tag"]
+        txid = line["txid"]
+        # Decrypt
+        decrypted = decAes(kyberct,nonce,cipherText,tag)
+
+        transactionData = {
+            "WalletAddress":line["publishers"],
+            "Node":line["Keys"],
+            "TransactionID":line["txid"]
+        }
+
 
 
 
