ae2da64871951c9173f57de36a92a737efd6a381
SamJones56 <B00156001@mytudublin.ie>
2025-03-26 10:52:06 +0000

    work on basic flaskk

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,21 @@
 # https://flask.palletsprojects.com/en/stable/quickstart/
 from flask import Flask
-from logReader import readDecryptSave
+from app.logReader import readDecryptSave
 
 
+decryptedFilepath = "streamDataDec.txt"
+# Start webpage
 # flask --app ../flask/app.py run --host=0.0.0.0 --port=8000
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    # Run readDecryptSave to get the current status of the file
+    readDecryptSave("streamDataDec.txt", "data")
+
+    with decryptedFilepath.open("r") as logFile:
+        for logLine in logFile:
+            return f"{logLine}"
+
+    # return "<p>Hello, World!</p>"
