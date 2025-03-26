cbf6e2b9e4e8335dbb72a24c11d2f1fcfc3589e0
LavenderNights <lavender_nights@protonmail.com>
2025-03-26 19:08:34 +0000

    Start on Watchdog service
    needs further work including file reading and integration with the rest of the project.

--- app/watchDog.py
+++ app/watchDog.py
@@ -1,1 +1,33 @@
 # https://pypi.org/project/watchdog/
+import time
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.events import FileModifiedEvent
+from watchdog.observers import Observer
+
+filePath=["/home/lilith/src/paper"]
+
+##Class for monitor rules, defines what happens when a file is modified or deleted using a pattern
+class Monitorclass(PatternMatchingEventHandler):
+    #pattern for monitoring pulled from filepath defined
+    patterns=filePath
+    #rules for printing that the file has been modified, to be replaced with reading the last line of the file
+    #and uploading it, with an exception for if there is no file anymore
+    def on_modified(self, event):
+            print(f"file modified: {event.src_path}")
+    
+    def on_deleted(self, event):
+            print(f"file deleted: {event.src_path}")
+
+event_handler = Monitorclass()
+observer= Observer()
+#code for the observer to only look in the path given, as well as recursive loop that keeps it going and allows keyboard interrupt.
+for path in filePath:
+    observer.schedule(event_handler, path, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+     observer.stop()
+observer.join
