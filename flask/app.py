bc3bd6669ede1bdead2ca72a7630b7f843aa6d51
SamJones56 <B00156001@mytudublin.ie>
2025-03-26 14:16:25 +0000

    work on flask

--- flask/app.py
+++ flask/app.py
@@ -1,28 +1,32 @@
 # https://flask.palletsprojects.com/en/stable/quickstart/
 from flask import Flask, jsonify, request, render_template
+# To use methods Sam wrote : from [name.py] import [method_name] e.g. look below this line
 from logReader import readDecryptSave
+from mcController import addToStream
 import json
 
 # USAGE
-# python3 ../flask/app.py
+# ./web.sh
 
-
+# Where stream data decrypted is located
 decryptedFilepath = "/logChain/app/streamDataDec.json"
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def displayLog():
     # Run readDecryptSave to get the current status of the file
     readDecryptSave("/logChain/app/streamDataDec.json", "data")
 
     # https://medium.com/@junpyoo50/transforming-json-input-into-html-table-view-with-flask-and-jinja-a-step-by-step-guide-1d62e2fa49ed
     # init logs and listkeys
     logs = []
-    listKeys = []
+    logKeys = []
     # Open decrypted file from blockchain
     with open(decryptedFilepath,"r") as logFile:
         # read through the open file
+        # log file = data of the entire file
+        # log line = each line in the logFile ~ for loop
         for logLine in logFile:
             try:
                 # save each of the lines as a json log entry
@@ -30,14 +34,25 @@ def hello_world():
                 # add logs to the logs variable
                 logs.append(logEntry)
                 # Set the list keys - used in table header
-                listKeys = list(logEntry.keys())
+                logKeys = list(logEntry.keys())
             
             # throw error
             except Exception as e:
                 print(f"e")
     # Render index.html with the data
-    return render_template('index.html', logs=logs, keys=listKeys)
+    return render_template('index.html', logs=logs, keys=logKeys)
 
 # https://stackoverflow.com/questions/29882642/how-to-run-a-flask-application
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8000")
+
+
+# def login():
+#     # user logs in and check for psswd
+#     username = ""
+#     loginData = {
+#         "username":username,
+#         "time":getTime()
+#     }
+    
+#     addToStream("login","genesis",loginData)
