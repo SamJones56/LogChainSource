16b36ab85d9acf68b3019df03381ba4df16fd7f8
LavenderNights <lavender_nights@protonmail.com>
2025-03-29 17:21:05 +0000

    File reading and file check implemented
    Needs integration with the rest of the project

--- app/watchDog.py
+++ app/watchDog.py
@@ -1,22 +1,31 @@
 # https://pypi.org/project/watchdog/
 import time
 from watchdog.events import PatternMatchingEventHandler
-from watchdog.events import FileModifiedEvent
 from watchdog.observers import Observer
+from sh import tail
+from pathlib import Path
+import os.path
 
 filePath=["/home/lilith/src/paper"]
-
+#converts paths from list to string and then converts to special path for file check
+pathString=''.join(map(str, filePath))
+checkPath= Path(pathString)
 ##Class for monitor rules, defines what happens when a file is modified or deleted using a pattern
 class Monitorclass(PatternMatchingEventHandler):
+    
     #pattern for monitoring pulled from filepath defined
     patterns=filePath
     #rules for printing that the file has been modified, to be replaced with reading the last line of the file
     #and uploading it, with an exception for if there is no file anymore
     def on_modified(self, event):
-            print(f"file modified: {event.src_path}")
-    
-    def on_deleted(self, event):
-            print(f"file deleted: {event.src_path}")
+            #check for if the file exists, else outputs an error
+            if checkPath.is_file():
+                #prints last line on unix system
+                for line in tail("-n 1",filePath, _iter=True):
+                     print(line) 
+            
+            else:
+                 print("oopsie, ya file got gone")
 
 event_handler = Monitorclass()
 observer= Observer()
